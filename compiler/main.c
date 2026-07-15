88c902279134cd68526ab16f4617a83944d9dca5
DodoSeal <maxhmcd08@gmail.com>
2026-07-15 09:33:21 -0600

    Compiler Project: Start Tokenizer

---
+++ compiler/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+// Compiler Types
+#include "types/Token.h"
+#include "types/Keyword.h"
+
+void Tokenize() {
+
+};
+
+int main() {
+    
+
+    return 0;
+};
