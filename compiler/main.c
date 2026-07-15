c07e756cf5bbfa1fa1f85b4d602db9ed447fba22
DodoSeal <maxhmcd08@gmail.com>
2026-07-15 13:42:34 -0600

    Compiler Project: Move Tokenizer out of main file

--- compiler/main.c
+++ compiler/main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-
-// Compiler Types
-#include "types/Token.h"
-#include "types/Keyword.h"
-
-void Tokenize() {
-
-};
+#include "tokenizer/tokenizer.h"
 
 int main() {
-    
+    Tokenize("const value = 1;");
 
     return 0;
 };
