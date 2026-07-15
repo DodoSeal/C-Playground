f0509d1f73feb556d0f6a60ff5796405db8b2037
DodoSeal <maxhmcd08@gmail.com>
2026-07-15 14:53:36 -0600

    Compiler Project: Create regex functions for tokenizer

--- compiler/tokenizer/tokenizer.c
+++ compiler/tokenizer/tokenizer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 int position = 0;
 int line = 0;
@@ -34,6 +35,112 @@ char advance() {
     if (is_eof() || (position + 1) > input_len) return '\0';
 
     return input[position++];
+};
+
+int is_number(char *data) {
+    regex_t regex;
+    int ret;
+
+    ret = regcomp(&regex, "^[0-9]+$", REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex pattern!");
+        return 0;
+    };
+
+    ret = regexec(&regex, data, 0, NULL, 0);
+    regfree(&regex);
+
+    return !ret;
+};
+
+int is_letter(char *data) {
+    regex_t regex;
+    int ret;
+
+    ret = regcomp(&regex, "^[a-zA-Z]+$", REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex pattern!");
+        return 0;
+    };
+
+    ret = regexec(&regex, data, 0, NULL, 0);
+    regfree(&regex);
+    
+    return !ret;
+};
+
+int is_operator(char *data) {
+    regex_t regex;
+    int ret;
+
+    ret = regcomp(&regex, "^[+*/%=!<>&|^~-]+$", REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex pattern!");
+        return 0;
+    };
+
+    ret = regexec(&regex, data, 0, NULL, 0);
+    regfree(&regex);
+    
+    return !ret;
+};
+
+int is_punctuation(char *data) {
+    regex_t regex;
+    int ret;
+
+    ret = regcomp(&regex, "^[;(){}[],.]+$", REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex pattern!");
+        return 0;
+    };
+
+    ret = regexec(&regex, data, 0, NULL, 0);
+    regfree(&regex);
+    
+    return !ret;
+};
+
+void skip_whitespace() {
+    regex_t regex;
+    int ret;
+    char data = peek(0);
+
+    ret = regcomp(&regex, "^[[:space:]]+$", REG_EXTENDED);
+    if (ret != 0) {
+        printf("Could not compile regex pattern!");
+        return;
+    };
+
+    ret = regexec(&regex, &data, 0, NULL, 0);
+    regfree(&regex);
+    
+    while (!ret) {
+        advance();
+    };
+};
+
+// TODO: Make this return a Token
+void read_next() {
+    skip_whitespace();
+
+    // TODO: Make EOF Token
+    // if (is_eof()) return;
+
+    char current = peek(0);
+    char next = peek(1);
+
+    if (current == "/" && next == "/") {
+        advance();
+        advance();
+
+        if (peek(0) == " ") advance();
+
+        // TODO: Handle comment tokens
+        // return;
+    };
+
+    
 };
 
 /**
