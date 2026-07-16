b79f1446e3626341f887a778713cdd8673d2a45b
DodoSeal <maxhmcd08@gmail.com>
2026-07-16 09:13:28 -0600

    Compiler Project: Add tokenization for digits and whitespace checking

--- compiler/tokenizer/tokenizer.c
+++ compiler/tokenizer/tokenizer.c
@@ -12,12 +12,18 @@ struct Tokenizer {
     size_t position;
     Token *tokens;
     size_t token_count;
+    int line;
+    int column;
 
     char (*peek)(Tokenizer *this, int offset);
     char (*advance)(Tokenizer *this);
     int (*is_eof)(Tokenizer *this);
+    void (*skip_whitespace)(Tokenizer *this);
+    Token* (*read_next)(Tokenizer *this);
+    Token* (*tokenize_comment)(Tokenizer *this);
+    Token* (*tokenize_digit)(Tokenizer *this);
 
-    Token (*run)(Tokenizer *this);
+    Token* (*run)(Tokenizer *this);
 };
 
 char peek(Tokenizer *this, int offset) {
@@ -39,8 +45,77 @@ int is_eof(Tokenizer *this) {
     return (this->position > this->input_len);
 };
 
+void skip_whitespace(Tokenizer *this) {
+    char next = this->peek(this, 0);
+    while (this->position < this->input_len && isspace(next)) {
+        this->advance(this);
+    };
+};
+
+// TODO
+Token *tokenize_comment(Tokenizer *this) {
+
+};
+
+Token *tokenize_digit(Tokenizer *this) {
+    char value[64] = "";
+
+    while (isdigit(this->peek(this, 0))) {
+        strcat(value, this->advance(this));
+    };
+
+    if (this->peek(this, 0) == '.' && isdigit(this->peek(this, 1))) {
+        this->advance(this);
+
+        while (isdigit(this->peek(this, 0))) {
+            strcat(value, this->advance(this));
+        };
+    };
+
+    Token digit_token = {
+        TOKEN_NUMBER,
+        value,
+        this->line,
+        this->column
+    };
+
+    return &digit_token;
+};
+
+Token *read_next(Tokenizer *this) {
+    this->skip_whitespace(this);
+    if (this->is_eof(this)) {
+        Token eof_token = {
+            TOKEN_EOF,
+            "",
+            this->line,
+            this->column
+        };
+
+        return &eof_token;
+    };
+
+    char current = this->peek(this, 0);
+    char next = this->peek(this, 1);
+
+    // TODO: Handle comments?
+    // this->tokenize_comment(this);
+
+    if (isdigit(current)) return this->tokenize_digit(this);
+};
+
 Token *run(Tokenizer *this) {
-    // TODO: Do tokenizer thing duhhhhh
+    Token *tokens[64] = {};
+
+    while (!this->is_eof(this)) {
+        Token *next = this->read_next(this);
+
+        if (next->type == TOKEN_EOF) break;
+
+        // TODO: somehow append next to tokens
+    };
+
+    return tokens;
 };
 
 Tokenizer *tokenizer_create(const char *input) {
@@ -60,10 +135,15 @@ Tokenizer *tokenizer_create(const char *input) {
     this->position = 0;
     this->token_count = 0;
     this->tokens = NULL;
+    this->line = 0;
+    this->column = 0;
 
     this->peek = peek;
     this->advance = advance;
     this->is_eof = is_eof;
+    this->skip_whitespace = skip_whitespace;
+    this->tokenize_comment = tokenize_comment;
+    this->tokenize_digit = tokenize_digit;
 
     this->run = run;
 
--- compiler/tokenizer/tokenizer.h
+++ compiler/tokenizer/tokenizer.h
@@ -4,18 +4,25 @@
 #define TOKENIZER_H_
 
 typedef struct Tokenizer Tokenizer;
+
 struct Tokenizer {
     char *input;
     size_t input_len;
     size_t position;
     Token *tokens;
     size_t token_count;
+    int line;
+    int column;
 
     char (*peek)(Tokenizer *this, int offset);
     char (*advance)(Tokenizer *this);
     int (*is_eof)(Tokenizer *this);
+    void (*skip_whitespace)(Tokenizer *this);
+    Token* (*read_next)(Tokenizer *this);
+    Token* (*tokenize_comment)(Tokenizer *this);
+    Token* (*tokenize_digit)(Tokenizer *this);
 
-    Token (*run)(Tokenizer *this);
+    Token* (*run)(Tokenizer *this);
 };
 
 Tokenizer *tokenizer_create(const char *input);
